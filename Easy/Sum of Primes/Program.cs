ed1df349e52baf0b278c76f6377e7c0cbd61ef3b
JosephHalim <joseph.e.halim@gmail.com>
2014-09-13 08:37:46 -0700

    Sum of Primes Question Added
    Added Sum of Primes Problem and Solution

---
+++ Easy/Sum of Primes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int sum = 0;
+        int prime = 0;
+        int count = 2;
+        do
+        {
+            int temp = isPrime(count);
+            if (temp > 0)
+            {
+                sum += temp;
+                prime++;
+            }
+            count++;
+        } while (prime != 1000);
+        Console.WriteLine(sum);
+    }
+    private static int isPrime(int num)
+    {
+
+        for (int i = 2; i < num; i++)
+        {
+            if (num % i == 0)
+            {
+                return 0;
+            }
+        }
+        return num;
+
+
+    }
+
+}
