d74b4ed8e1a9d6ebd17c7af6c0141477bb463574
JosephHalim <joseph.e.halim@gmail.com>
2014-09-14 09:39:23 -0700

    Added Multiple of Number & Reverse String
    Added Problem, Text File, and Solutions for Multiple of Number and
    Reverse String

---
+++ Easy/Multiple of Numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (StreamReader reader = File.OpenText(@"C:\Users\Joey\Desktop\Multiple.txt"   ))
+        while (!reader.EndOfStream)
+        {
+            string[] line = reader.ReadLine().Split(',');
+            if (null == line)
+                continue;
+            int counter = 1;
+            int largest = 0;
+            do{
+                 largest =  Convert.ToInt32(line[1]) * counter;
+                 counter++;
+                
+            }
+            while(Convert.ToInt32(line[0])>largest);
+          
+            Console.WriteLine(largest);
+           
+        }
+        Console.ReadLine();
+    }
+}
---
+++ Easy/Reverse String/Reverse String Program.cs
@@ -0,0 +1,36 @@
+﻿
+using System.IO;
+using System.Collections.Generic;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (StreamReader reader = File.OpenText(args[0]))
+        while (!reader.EndOfStream)
+        {
+            string[] line = reader.ReadLine().Split(' ');
+            int counter=0;
+            if (null == line)
+                continue;
+            do 
+            {
+                string temp = null;
+                temp = line[counter];
+                line[counter] = line[line.Length - counter-1];
+                line[line.Length-1 - counter] = temp;
+                counter++;
+            } while (line.Length - counter > counter);
+            int x = 0;
+            do
+            {
+                Console.Write(line[x] + ' ');
+                x++;
+            } while (x < line.Length);
+            Console.WriteLine();
+        }
+        Console.ReadLine();
+    }
+    
+}
