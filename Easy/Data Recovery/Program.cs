8ff24fe25499dc1a7ad19a0b8aeedef72f238757
JosephHalim <joseph.e.halim@gmail.com>
2014-09-18 08:37:05 -0700

    Added Data Recovery and Letter Percentage Problem
    Added Data Recovery and Letter Percentage, problem, test file, and
    solution

---
+++ Easy/Data Recovery/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Data_Recovery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> data = new Dictionary<int, string>();
+            using(StreamReader reader = File.OpenText(args[0]))
+            while (!reader.EndOfStream)
+            {              
+                string[] line = reader.ReadLine().Split(';');
+                string[] word = line[0].Split(' ');
+                string[] place = line[1].Split(' ');
+                var numberarray = new List<int>();
+               
+                for (int x = 0; x < word.Length+1;x++ )
+                {             
+                    numberarray.Add(x);
+                }
+            
+                for (int x = 0; x < word.Length; x++)
+                    {
+                        try
+                        {
+                            data.Add(Convert.ToInt32(place[x]), Convert.ToString(word[x]));
+                            numberarray.Remove(Convert.ToInt32(place[x]));                       
+                        }
+                        catch (System.IndexOutOfRangeException)
+                        {
+                            var temp = numberarray.ToArray();
+                            data.Add(Convert.ToInt32(temp[1]), Convert.ToString(word[x]));
+                        }
+                    }
+              var items = from keyvalue in data
+                  orderby keyvalue.Key ascending
+                      select keyvalue;
+               
+                foreach(KeyValuePair<int,string> key in items)
+                {
+                    Console.Write(key.Value + ' ');
+                }
+                Console.WriteLine();
+                Array.Clear(word, 0, word.Length);
+                Array.Clear(place, 0, place.Length);
+                data.Clear();
+            }
+            Console.ReadLine();
+        }   
+    }
+}
+
---
+++ Easy/Letter Percentage/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace LetterPercentage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = File.OpenText(args[0]))
+            while(!reader.EndOfStream)
+            {
+                string lowercase = "abcdefghijklmnopqrstuvwxyz";
+                float numlower = 0;
+                float numupper = 0;
+                string line = reader.ReadLine();
+                if (null == line)
+                    continue;
+                for(int x = 0; x<line.Length; x++)
+                {
+                 
+                    if (lowercase.Contains(line.Substring(x,1)) == true)
+                    {
+                        
+                        numlower++;
+                    }
+                    else
+                    {
+                        numupper++;
+                    }
+                }
+               
+                Console.WriteLine("lowercase: " + ((numlower / (numlower + numupper))*100).ToString("F02") + " uppercase: " + ((numupper / (numlower + numupper))*100).ToString("F02"));
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
