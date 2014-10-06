5c0bd8228beeb93d11c39305423253206d35a392
JosephHalim <joseph.e.halim@gmail.com>
2014-10-06 08:33:56 -0700

    Added Base 16 and Mod Problem
    Added solution, questions, and sample data for 2 more quesitons

---
+++ Easy/Convert16-10/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Mod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = File.OpenText(args[0]))
+            while(!reader.EndOfStream)
+            {
+                var data = reader.ReadLine().Split(',');
+                int multiple = Convert.ToInt32(data[0]) / Convert.ToInt32(data[1]);
+              Console.WriteLine( Convert.ToInt32(data[0]) - (multiple * Convert.ToInt32(data[1])));
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
